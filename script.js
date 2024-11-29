e756900403193880e690f2e00aa319727f1cedf3
Schuasta1985 <thomas.schuster-vb@eclipso.at>
2024-11-29 06:53:41 +0100

    Update script.js
    function animate

--- script.js
+++ script.js
@@ -80,42 +80,35 @@ function zeigeLevelUpAnimation() {
         }
     }
 
-    function animate() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-        gradient.addColorStop(0, 'red');
-        gradient.addColorStop(0.5, 'yellow');
-        gradient.addColorStop(1, 'green');
-        ctx.fillStyle = gradient;
-        ctx.fillRect(100, canvas.height / 2 - 20, (progress / maxProgress) * (canvas.width - 200), 40);
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop(0, 'red');
+    gradient.addColorStop(0.5, 'yellow');
+    gradient.addColorStop(1, 'green');
+    ctx.fillStyle = gradient;
+    ctx.fillRect(100, canvas.height / 2 - 20, (progress / maxProgress) * (canvas.width - 200), 40);
 
-        if (progress === maxProgress) {
-            drawFireworks();
-            if (fireworks.length === 0) {
-                fireworks = createFirework(canvas.width / 2, canvas.height / 2);
-            }
-            explosionRadius += 5;
-            if (fireworks.length === 0) {
-                showLevelUp();
-                return;
-            }
-        }
-
-        progress += 1;
-        if (progress < maxProgress) {
-            requestAnimationFrame(animate);
-        } else {
-            requestAnimationFrame(() => {
-                drawFireworks();
-                if (fireworks.length > 0) {
-                    requestAnimationFrame(animate);
-                } else {
-                    showLevelUp();
-                }
-            });
-        }
+    if (progress === maxProgress) {
+        fireworks = createFirework(canvas.width / 2, canvas.height / 2);
+        drawFireworks();
     }
+
+    progress += 1;
+    if (progress < maxProgress) {
+        requestAnimationFrame(animate);
+    } else {
+        requestAnimationFrame(() => {
+            drawFireworks();
+            if (fireworks.length > 0) {
+                requestAnimationFrame(animate);
+            } else {
+                showLevelUp();
+            }
+        });
+    }
+}
 
     function showLevelUp() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
